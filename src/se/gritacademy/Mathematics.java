0e32839d4a86a11320df46ff7230d13458ffc064
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-11-05 23:50:24 +0100

    Added 16th test

--- src/se/gritacademy/Mathematics.java
+++ src/se/gritacademy/Mathematics.java
@@ -2,49 +2,43 @@ package se.gritacademy;
 
 public class Mathematics {
 
-    public long minLong(long val, long val2){
-        return Math.min(val, val2);
+    public long minLong(long n1, long n2){
+        return Math.min(n1, n2);
     }
-
-    public float minFloat(float val, float val2){
-        return Math.min(val,val2);
+    public float minFloat(float n1, float n2){
+        return Math.min(n1,n2);
     }
-    public int minInteger(int val, int val2){
-        return Math.min(val,val2);
+    public int minInteger(int n1, int n2){
+        return Math.min(n1,n2);
     }
-    public double minDouble(double val, double val2){
-        return Math.min(val,val2);
+    public double minDouble(double n1, double n2){
+        return Math.min(n1,n2);
     }
-    public double powerOff(double val, double val2){
-        return Math.pow(val,val2);
+    public double powerOff(double base, double exponent){
+        return Math.pow(base,exponent);
     }
-    public int valueIntegerPlusOne(int val){
-        return val + 1;
+    public int valueIntegerPlusOne(int base){
+        return base + 1;
     }
-    public long valueLongPlusOne(long val){
-        return val + 1L;
+    public long valueLongPlusOne(long base){
+        return base + 1L;
     }
-    public int valueIntegerMinusOne(int val){
-        return val - 1;
+    public int valueIntegerMinusOne(int base){
+        return base - 1;
     }
-    public long valueLongMinusOne(long val){
-        return val - 1L;
+    public long valueLongMinusOne(long base){
+        return base - 1L;
     }
-    public double squaredOff(double val) {
-        return Math.sqrt(val);
+    public double squaredOf(double sqr) {
+        return Math.sqrt(sqr);
     }
-    public double cubicValueOff(double val){
-        return Math.cbrt(val);
-    }
-    public double randomValue(double val){
-        return Math.random() * val;
+    public double cubicValueOf(double cubic){
+        return Math.cbrt(cubic);
     }
 
 
 
 
     public static void main(String[] args) {
-        Mathematics math = new Mathematics();
-        System.out.println(math.cubicValueOff(120.00));
     }
 }
--- src/se/gritacademy/MathematicsTest.java
+++ src/se/gritacademy/MathematicsTest.java
@@ -1,82 +1,87 @@
 package se.gritacademy;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.DoubleStream;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 class MathematicsTest {
 
     Mathematics math = new Mathematics();
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testMinLong() {
         assertEquals(1000000000L,math.minLong(2000000000L,1000000000L));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testMinFloat() {
         assertEquals(1.2345f,math.minFloat(3.212f,1.2345f));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testMinInteger() {
         assertEquals(123,math.minInteger(273273,123));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testMinDouble() {
         assertEquals(12.00d,math.minDouble(16.02d,12.00d));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testPowerOff() {
         assertEquals(36.00d,math.powerOff(6.00d,2.00d));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testValueIntegerPlusOne() {
         assertEquals(4, math.valueIntegerPlusOne(3));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testValueLongPlusOne() {
         assertEquals(3000000001L,math.valueLongPlusOne(3000000000L));
     }
-    @org.junit.jupiter.api.Test
+    @Test
     void testValueIntegerMinusOne() {
         assertEquals(2, math.valueIntegerMinusOne(3));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testValueLongMinusOne() {
         assertEquals(2999999999L,math.valueLongMinusOne(3000000000L));
     }
 
-    @org.junit.jupiter.api.Test
-    void testSquaredOff() {
-        assertEquals(6.00d,math.squaredOff(36.00d));
+    @Test
+    void testSquaredOf() {
+        assertEquals(6.00d,math.squaredOf(36.00d));
     }
 
-    @org.junit.jupiter.api.Test
-    void testCubicValueOff() {
-        assertEquals(4.932424148660941d, math.cubicValueOff(120.00));
+    @Test
+    void testCubicValueOf() {
+        assertEquals(4.932424148660941d, math.cubicValueOf(120.00d));
 
     }
-    
+
+    /*
+    *  setting delta at 0.9d so that run time ends at some point
+    *  set MathematicsTest.randomValue[*] in configuration for endless loop until failure
+    */
+
     @ParameterizedTest
     @MethodSource("randomNumber")
     void randomValue(double number) {
-        Assertions.assertTrue(number < 10);
-        
+        assertEquals(0.0d,number,0.9d);
+        System.out.println(number);
     }
     static DoubleStream randomNumber(){
-        return DoubleStream.of(Math.random() * 10);
+        return DoubleStream.of(Math.random());
     }
+
 }
