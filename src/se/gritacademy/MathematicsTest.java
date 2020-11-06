486184232da5c9f33b32778ed0d608a13e0984a8
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-11-06 08:51:12 +0100

    revamped initial commit a bit

--- src/se/gritacademy/MathematicsTest.java
+++ src/se/gritacademy/MathematicsTest.java
@@ -1,6 +1,7 @@
 package se.gritacademy;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,61 +12,91 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class MathematicsTest {
+    private Mathematics math;
 
-    Mathematics math = new Mathematics();
-
+    @BeforeEach
+    void setUp(){
+        math = new Mathematics();
+    }
     @Test
     void testMinLong() {
-        assertEquals(1000000000L,math.minLong(2000000000L,1000000000L));
+        long n1 = 2000000000L;
+        long n2 = 1000000000L;
+        long expected = 1000000000L;
+        assertEquals(expected,math.minLong(n1,n2));
     }
 
     @Test
     void testMinFloat() {
-        assertEquals(1.2345f,math.minFloat(3.212f,1.2345f));
+        float n1 = 3.212f;
+        float n2 = 1.2345f;
+        float expected = 1.2345f;
+        assertEquals(expected,math.minFloat(n1,n2));
     }
 
     @Test
     void testMinInteger() {
-        assertEquals(123,math.minInteger(273273,123));
+        int n1 = 273273;
+        int n2 = 123;
+        int expected = 123;
+        assertEquals(expected,math.minInteger(n1,n2));
     }
 
     @Test
     void testMinDouble() {
-        assertEquals(12.00d,math.minDouble(16.02d,12.00d));
+        double n1 = 16.02d;
+        double n2 = 12.00d;
+        double expected = 12.00d;
+        assertEquals(expected,math.minDouble(n1,n2));
     }
 
     @Test
     void testPowerOff() {
-        assertEquals(36.00d,math.powerOff(6.00d,2.00d));
+        double base = 6.00d;
+        double exponent = 2.00d;
+        double expected = 36.00d;
+        assertEquals(expected,math.powerOff(base,exponent));
     }
 
     @Test
     void testValueIntegerPlusOne() {
-        assertEquals(4, math.valueIntegerPlusOne(3));
+        int base = 3;
+        int expected = 4;
+        assertEquals(expected, math.valueIntegerPlusOne(base));
     }
 
     @Test
     void testValueLongPlusOne() {
-        assertEquals(3000000001L,math.valueLongPlusOne(3000000000L));
+        long base = 3000000000L;
+        long expected = 3000000001L;
+        assertEquals(expected,math.valueLongPlusOne(base));
     }
     @Test
     void testValueIntegerMinusOne() {
-        assertEquals(2, math.valueIntegerMinusOne(3));
+        int base = 3;
+        int expected = 2;
+        assertEquals(expected, math.valueIntegerMinusOne(base));
     }
 
     @Test
     void testValueLongMinusOne() {
-        assertEquals(2999999999L,math.valueLongMinusOne(3000000000L));
+        long base = 3000000000L;
+        long expected = 2999999999L;
+        assertEquals(expected,math.valueLongMinusOne(base));
     }
 
     @Test
     void testSquaredOf() {
-        assertEquals(6.00d,math.squaredOf(36.00d));
+        double sqr = 36.00d;
+        double expected = 6.00d;
+        assertEquals(expected,math.squaredOf(sqr));
     }
 
     @Test
     void testCubicValueOf() {
-        assertEquals(4.932424148660941d, math.cubicValueOf(120.00d));
+        double cubic = 120.00d;
+        double expected = 4.932424148660941d;
+        assertEquals(expected, math.cubicValueOf(cubic));
 
     }
 
@@ -77,8 +108,9 @@ class MathematicsTest {
     @ParameterizedTest
     @MethodSource("randomNumber")
     void randomValue(double number) {
-        assertEquals(0.0d,number,0.9d);
-        System.out.println(number);
+        double expected = 0.0d;
+        double delta = 0.9d;
+        assertEquals(expected,number,delta);
     }
     static DoubleStream randomNumber(){
         return DoubleStream.of(Math.random());
